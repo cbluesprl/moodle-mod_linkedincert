d66bdafc82917cbce49e3c029720602e34d65833
Renaud Lemaire <rlemaire@cblue.be>
2021-03-24 10:50:29 +0100

    #328751 review lang/en.php

--- lang/en/linkedincert.php
+++ lang/en/linkedincert.php
@@ -32,35 +32,35 @@ $string['modulename'] = 'LinkedIn certificates Activity';
 $string['modulename_link'] = 'mod/linkedincert/view';
 $string['modulenameplural'] = 'LinkedIn certificates Activities';
 
-$string['linkedincertsettings'] = 'LinkedIn Certificates Settings';
-$string['organizationid'] = 'Organization id';
-$string['organizationid_help'] = 'Linkedin ID of your organization<br /><div style="margin-bottom:15px; padding: 0 15px; border:1px solid #dee2e6 !important"><h3>How to find your LinkedIn Organization ID?</h3><ol><li>Log in to LinkedIn as the admin for your business\' Organisation Page</li><li>Check the URL used when you are logged in as the admin</li><li>Your LinkedIn Organization ID is the seven-digit number in the URL :<br/><i>https://www.linkedin.com/company/ORGANIZATION_ID/admin/</i></li></ol></div>';
-$string['organizationname'] = 'Organization name';
-$string['organizationname_help'] = 'If you do not have an Organization id, this field will be used instead';
-
-
-$string['receiveddate'] = 'Awarded on';
-$string['listofissues'] = 'Recipients: {$a}';
-$string['nocerts'] = 'There are no certificates for this course';
-
 $string['awardedto'] = 'Awarded to';
 $string['cannotverifyallcertificates'] = 'You do not have the permission to verify all certificates on the site.';
 $string['certificate'] = 'Certificate';
 $string['code'] = 'Code';
-$string['verified'] = 'Verified';
+$string['coursetimereq_help'] = 'Enter here the minimum amount of time, in minutes, that a student must be logged into the course before they will be able to receive
+the certificate.';
+$string['coursetimereq'] = 'Required minutes in course';
+$string['deleteconfirm'] = 'Delete confirmation';
+$string['deleteissueconfirm'] = 'Are you sure you want to delete this certificate issue?';
+$string['deleteissuedcertificates'] = 'Delete issued certificates';
+$string['invalidcode'] = 'Invalid code supplied.';
+$string['linkedincertsettings'] = 'LinkedIn Certificates Settings';
+$string['listofissues'] = 'Recipients: {$a}';
+$string['nocerts'] = 'There are no certificates for this course';
 $string['notissued'] = 'Not awarded';
 $string['notverified'] = 'Not verified';
-$string['verify'] = 'Verify';
-$string['verifyallcertificates'] = 'Allow verification of all certificates';
-$string['verifyallcertificates_desc'] = 'When this setting is enabled any person (including users not logged in) can visit the link \'{$a}\' in order to verify any certificate on the site, rather than having to go to the verification link for each certificate.
-
-Note - this only applies to certificates where \'Allow anyone to verify a certificate\' has been set to \'Yes\' in the certificate settings.';
-$string['verifycertificate'] = 'Verify certificate';
-$string['verifycertificatedesc'] = 'This link will take you to a new screen where you will be able to verify certificates on the site';
-$string['verifycertificateanyone'] = 'Allow anyone to verify a certificate';
-$string['verifycertificateanyone_help'] = 'This setting enables anyone with the certificate verification link (including users not logged in) to verify a certificate.';
-
-$string['coursetimereq'] = 'Required minutes in course';
 $string['options'] = 'Options';
-$string['verifycertificateanyone'] = 'Allow anyone to verify a certificate';
+$string['organizationid_help'] = 'Linkedin ID of your organization<br /><div style="margin-bottom:15px; padding: 0 15px; border:1px solid #dee2e6 !important"><h3>How to find your LinkedIn Organization ID?</h3><ol><li>Log in to LinkedIn as the admin for your business\' Organisation Page</li><li>Check the URL used when you are logged in as the admin</li><li>Your LinkedIn Organization ID is the seven-digit number in the URL :<br/><i>https://www.linkedin.com/company/ORGANIZATION_ID/admin/</i></li></ol></div>';
+$string['organizationid'] = 'Organization id';
+$string['organizationname_help'] = 'If you do not have an Organization id, this field will be used instead';
+$string['organizationname'] = 'Organization name';
+$string['receiveddate'] = 'Awarded on';
+$string['requiredtimenotmet'] = 'You must spend at least a minimum of {$a->requiredtime} minutes in the course before you can access this certificate.';
+$string['verified'] = 'Verified';
+$string['verify'] = 'Verify';
+$string['verifyallcertificates_desc'] = 'When this setting is enabled any person (including users not logged in) can visit the link \'{$a}\' in order to verify any certificate on the site, rather than having to go to the verification link for each certificate.
+Note - this only applies to certificates where \'Allow anyone to verify a certificate\' has been set to \'Yes\' in the certificate settings.';
+$string['verifyallcertificates'] = 'Allow verification of all certificates';
+$string['verifycertificate'] = 'Verify certificate';
 $string['verifycertificateanyone_help'] = 'This setting enables anyone with the certificate verification link (including users not logged in) to verify a certificate.';
+$string['verifycertificateanyone'] = 'Allow anyone to verify a certificate';
+$string['verifycertificatedesc'] = 'This link will take you to a new screen where you will be able to verify certificates on the site';
